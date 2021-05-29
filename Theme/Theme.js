18cf57798bbf8f7adc2ca8d5b63660f8404bd8c1
cartolanofacundo <cartolanofacundo@gmail.com>
2021-05-28 21:51:41 -0300

    Theme update

---
+++ Theme/Theme.js
@@ -0,0 +1,20 @@
+export default theme = {
+    colors: {
+      primary: '#7350FF',
+      secondary: '#0081CC',
+      white: 'black',
+      black: 'black',
+      grey0: 'black',
+      grey1: 'black',
+      grey2: 'black',
+      grey3: 'black',
+      grey4: 'black',
+      grey5: 'black',
+      greyOutline: 'black',
+      searchBg: 'black',
+      success: 'black',
+      error: 'black',
+      warning: 'black',
+      divider: 'black'
+    }
+  }
