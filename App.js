1f3d81dceddb796d9c9b9b7472e2608df13d705d
cartolanofacundo <cartolanofacundo@gmail.com>
2021-05-30 10:45:42 -0300

    Settings for react native elements

--- App.js
+++ App.js
@@ -1,17 +1,24 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { HomeNavigator } from './routes/HomeStack';
 import { LoginNavigator } from './routes/LoginStack';
+import { ThemeProvider } from 'react-native-elements';
+import { Theme } from './Theme/Theme';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function App() {
 
   const singedIn = true;
 
   return (
-    <NavigationContainer>
-      {singedIn ? (<HomeNavigator/>) : (<LoginNavigator/>)}
-    </NavigationContainer>
+    <ThemeProvider theme={Theme}>
+      <SafeAreaProvider>
+        <NavigationContainer>
+          {singedIn ? (<HomeNavigator />) : (<LoginNavigator />)}
+        </NavigationContainer>
+      </SafeAreaProvider>
+    </ThemeProvider>
   );
 }
 
