3b2932ce7c7d0c8d2a2c0403b0d58eb4be7753a0
maxzbinden <maxi.zbinden@gmail.com>
2021-05-31 22:31:55 -0300

    cambios en Registration

--- components/Registration/Registration.js
+++ components/Registration/Registration.js
@@ -1,14 +1,29 @@
 import React from "react";
 import { View } from "react-native";
+import { CheckBox } from "react-native-elements/dist/checkbox/CheckBox";
 
 export const Registration = () => {
   return (
     <View>
       <InputCustom placeholder="Nombre"></InputCustom>
+
       <InputCustom placeholder="Apellido"></InputCustom>
+
       <InputCustom placeholder="Email"></InputCustom>
+
       <InputCustom placeholder="Contraseña"></InputCustom>
+
       <InputCustom placeholder="Confirmación contraseña"></InputCustom>
+
+      <CheckBox title="Acepto términos y condiciones" />
+
+      <Button
+        title="Registrarse"
+        buttonStyle={styles.button}
+        onPress={handleSubmit}
+        loading={loading}
+        disabled={buttonDisabled}
+      ></Button>
     </View>
   );
 };
