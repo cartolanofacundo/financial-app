bd878220cacaac97ea2b19b3d9cfbb995cdc6ffa
maxzbinden <maxi.zbinden@gmail.com>
2021-06-27 11:39:40 -0300

    CategoryStack

--- routes/CategoryStack.js
+++ routes/CategoryStack.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { Select } from '../Components/Categories/Select';
+import { CreateCategory } from '../Components/Categories/CreateCategory';
 
 
 const CategoryStack = createStackNavigator();
@@ -8,8 +9,10 @@ const CategoryStack = createStackNavigator();
 export const CategoryNavigator = () => {
 
   return (
-    <CategoryStack.Navigator initialRouteName="Select" headerMode="screen">
+    <CategoryStack.Navigator initialRouteName="Select" headerMode="none" mode="modal">
       <CategoryStack.Screen name="Select" component={Select} />
+      <CategoryStack.Screen name="CreateCategory" component={CreateCategory} />
+
 
     </CategoryStack.Navigator>
   );
