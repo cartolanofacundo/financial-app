4d4c1254eda514faf61136a444cb1c1749e16225
maxzbinden <maxi.zbinden@gmail.com>
2021-06-27 18:15:46 -0300

    Cambios CreateCategory

--- components/Categories/CreateCategory.js
+++ components/Categories/CreateCategory.js
@@ -2,12 +2,14 @@ import React, { useState } from "react";
 import { Text, Button, InputCustom } from "react-native-elements";
 import { View } from "react-native";
 import { CategoriesList } from "./CategoriesList";
+import { iconList } from "./IconList";
+import { Theme } from "../../Theme/Theme";
 
-export default function CreateCategory() {
+export function CreateCategory() {
   const [title, setTitle] = useState("");
   const [icon, setIcon] = useState((type = "material"), (name = "super"));
   const [newCategory, setNewCategory] = useState("");
-  const [newIcon, setNewIcon] = useState({type: "material", name: "new"})
+  const [newIcon, setNewIcon] = useState({ type: "font-awesome", name: "new" });
 
   const inputCategory = () => {
     setNewCategory();
@@ -19,8 +21,18 @@ export default function CreateCategory() {
     } else {
       setErrorMessage("");
       setLoading(true);
-    //   CategoriesList tiene que agregarla
+      //   iconList tiene que agregarla
+      CategoriesList = [...CategoriesList, {title: newCategory,icon: newIcon}];
     }
+  };
+
+  const categoryIcons = iconList.map((icon) => {
+    return icon.name;
+  });
+
+  const handleOnchangeIcon = (selectedIndex) => {
+    setSelectedIndex(selectedIndex);
+    setNewIcon(iconList[selectedIndex])
   };
 
   return (
@@ -28,21 +40,49 @@ export default function CreateCategory() {
       <View>
         <Text>Nueva categoría</Text>
         <Text>
-          Seleccioná un emoji presionando el "+" y luego ingresá un nombre para
-          identificarla
+          Seleccioná un emoji y luego ingresá un nombre para identificarla
         </Text>
       </View>
+
+      <ScrollView
+        style={styles.accountContainer}
+        horizontal={true}
+        showsHorizontalScrollIndicator={false}
+      >
+        <ButtonGroup
+          buttons={categoryIcons}
+          buttonStyle={{
+            width: 100,
+            borderColor: Theme.colors.primary,
+            borderWidth: 1,
+            borderRadius: 30,
+            margin: 0,
+            padding: 0,
+          }}
+          buttonContainerStyle={{ marginRight: 10 }}
+          containerStyle={{
+            borderColor: "transparent",
+            backgroundColor: "transparent",
+            margin: 0,
+            padding: 0,
+          }}
+          innerBorderStyle={{ color: "transparent" }}
+          selectedButtonStyle={{ backgroundColor: Theme.colors.primary }}
+          selectedIndex={selectedIndex}
+          onPress={(selectedIdx) => handleOnchangeIcon(selectedIdx)}
+        />
+      </ScrollView>
+
       <InputCustom
         placeholder="Nombre de la categoría"
         onChangeText={inputCategory}
       ></InputCustom>
-        <Button
-          title="Guardar"
-          buttonStyle={styles.button}
-          onPress={handleSubmit}
-          loading={loading}
-        ></Button>
-      
+      <Button
+        title="Guardar"
+        buttonStyle={styles.button}
+        onPress={handleSubmit}
+        loading={loading}
+      ></Button>
     </View>
   );
 }
---
+++ components/Categories/IconList.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+export const icons = [
+    {
+        type: "font-awesome",
+        name: "university",
+    },
+    {
+        type: "font-awesome",
+        name: "university",
+    },
+    {
+        type: "font-awesome",
+        name: "university",
+    },
+    {
+        type: "font-awesome",
+        name: "university",
+    },
+    {
+        type: "font-awesome",
+        name: "university",
+    },
+    {
+        type: "font-awesome",
+        name: "university",
+    },
+    {
+        type: "font-awesome",
+        name: "university",
+    },
+  ];
+ 
