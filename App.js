3e3934b62723f70dfad16b8ec856f8e0e90f1a51
Indira <ibriggiler@gmail.com>
2021-07-11 10:57:27 -0300

    Mergin

--- App.js
+++ App.js
@@ -1,34 +1,11 @@
 import React from "react";
-import { StyleSheet } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
-import { HomeNavigator } from "./routes/HomeStack";
-import { LoginNavigator } from "./routes/LoginStack";
-import { ThemeProvider } from "react-native-elements";
-import { Theme } from "./Theme/Theme";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import { TransactionsNavigator } from "./routes/TransactionsStack";
-import { RootNavigator } from "./routes/Root";
-import { AboutUsNavigator } from "./routes/AboutUsStack";
+import { RootStack } from "./routes/RootStack";
 
 export default function App() {
-  const singedIn = false;
-
   return (
     <SafeAreaProvider>
-      <NavigationContainer>
-        {/* {singedIn ? <RootNavigator /> : <LoginNavigator />} */}
-        {/* <TransactionsNavigator /> */}
-        <AboutUsNavigator/>
-      </NavigationContainer>
+      <RootStack />
     </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
--- routes/RootStack.js
+++ routes/RootStack.js
@@ -1,44 +1,44 @@
-import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
-import { TransactionStack } from './TransactionStack';
-import { NewTransactionScreen } from '../Components/NewTransaction/NewTransactionScreen';
-import { AppStack } from './AppStack';
+import { TransactionStack } from "./TransactionStack";
+import { AppStack } from "./AppStack";
 
 const Stack = createStackNavigator();
 
 export const RootStack = () => {
   return (
-
     <NavigationContainer>
-      <Stack.Navigator mode="modal" headerMode="none" screenOptions={{ animationEnabled: false }}>
+      <Stack.Navigator
+        mode="modal"
+        headerMode="none"
+        screenOptions={{ animationEnabled: false }}
+      >
         <Stack.Screen
           name="Main"
           component={AppStack}
           options={{ headerShown: false }}
         />
-        <Stack.Screen name="New Transaction"
+        <Stack.Screen
+          name="New Transaction"
           component={TransactionStack}
           options={{
             animationEnabled: true,
-            cardStyle:{
-              backgroundColor: "rgba(0,0,0,0.15)"
+            cardStyle: {
+              backgroundColor: "rgba(0,0,0,0.15)",
             },
             cardOverlayEnabled: true,
-            cardStyleInterpolator: ({current: {progress}}) => {
-              return{
+            cardStyleInterpolator: ({ current: { progress } }) => {
+              return {
                 cardStyle: {
-                  backgroundColor: "transparent"
-                  // opacity: progress.interpolate({
-                  //   inputRange: [0, 0.5, 0.9, 1],
-                  //   outputRange: [0, 0.25, 0.7, 1]
-                  // })
+                  backgroundColor: "transparent",
                 },
-              }
-            }
-          }} />
+              };
+            },
+          }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
-}
+};
